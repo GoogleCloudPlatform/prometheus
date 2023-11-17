cbfe5c3a45806371bd73f89197a6a99c32240ea9
Ridwan Sharif <ridwanmsharif@google.com>
2023-11-17 20:02:44 +0000

    scrape: add option to Manager to skip initial discovery reload
    This config option is quite useful on serverless environments where
    we are sensitive to the start up latencies of the scraper. The
    serverless instance might only last for a few seconds and may not be
    able to afford the minimum 5s reload interval before the scrape pools
    are created.
    
    Signed-off-by: Ridwan Sharif <ridwanmsharif@google.com>

--- scrape/manager.go
+++ scrape/manager.go
@@ -85,6 +85,12 @@ type Options struct {
 	EnableCreatedTimestampZeroIngestion bool
 	// Option to enable the ingestion of native histograms.
 	EnableNativeHistogramsIngestion bool
+	// Option to enable discovering targets immediately on start up as opposed
+	// to waiting for the interval defined in DiscoveryReloadInterval before
+	// initializing the scrape pools. Disabled by default. Useful for serverless
+	// flavors of OpenTelemetry contrib's prometheusreceiver where we're
+	// sensitive to start up delays.
+	DiscoveryReloadOnStartup bool
 
 	// Optional HTTP client options to use when scraping.
 	HTTPClientOptions []config_util.HTTPClientOption
@@ -157,8 +163,17 @@ func (m *Manager) reloader() {
 		reloadIntervalDuration = model.Duration(5 * time.Second)
 	}
 
-	ticker := time.NewTicker(time.Duration(reloadIntervalDuration))
+	// Skip the initial reload interval wait for the first reload.
+	if m.opts.DiscoveryReloadOnStartup {
+		select {
+		case <-m.triggerReload:
+			m.reload()
+		case <-m.graceShut:
+			return
+		}
+	}
 
+	ticker := time.NewTicker(time.Duration(reloadIntervalDuration))
 	defer ticker.Stop()
 
 	for {
