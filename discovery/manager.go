3c02fc8563fc2c52280d6f564453ce90edbe08c8
Ridwan Sharif <ridwanmsharif@google.com>
2023-11-18 01:27:28 +0000

    discovery: add optional skip ability for serverless environments
    This change adds an option that will allow users to skip the initial
    wait before sending target sets to the scrape manager. This is
    particularly useful in environments where the startup latency is
    required to be low just as in serverless deployments.
    
    Signed-off-by: Ridwan Sharif <ridwanmsharif@google.com>

--- discovery/manager.go
+++ discovery/manager.go
@@ -130,6 +130,16 @@ func Updatert(u time.Duration) func(*Manager) {
 	}
 }
 
+// SkipInitialWait sets the name of the manager. This is used in serverless flavours of OTel's prometheusreceiver
+// which is sensitive to startup latencies.
+func SkipInitialWait() func(*Manager) {
+	return func(m *Manager) {
+		m.mtx.Lock()
+		defer m.mtx.Unlock()
+		m.skipStartupWait = true
+	}
+}
+
 // HTTPClientOptions sets the list of HTTP client options to expose to
 // Discoverers. It is up to Discoverers to choose to use the options provided.
 func HTTPClientOptions(opts ...config.HTTPClientOption) func(*Manager) {
@@ -160,6 +170,11 @@ type Manager struct {
 	// How long to wait before sending updates to the channel. The variable
 	// should only be modified in unit tests.
 	updatert time.Duration
+
+	// skipStartupWait allows the discovery manager to skip the initial wait before sending updates
+	// to the channel. This is used in serverless flavours of OTel's prometheusreceiver
+	// which is sensitive to startup latencies.
+	skipStartupWait bool
 
 	// The triggerSend channel signals to the Manager that new updates have been received from providers.
 	triggerSend chan struct{}
@@ -350,6 +365,33 @@ func (m *Manager) updater(ctx context.Context, p *Provider, updates chan []*targ
 func (m *Manager) sender() {
 	ticker := time.NewTicker(m.updatert)
 	defer ticker.Stop()
+
+	// Send the targets downstream as soon as you see them if skipStartupWait is
+	// set. If discovery receiver's channel is too busy, fall back to the
+	// regular loop.
+	if m.skipStartupWait {
+		select {
+		case <-m.triggerSend:
+			m.metrics.SentUpdates.Inc()
+			select {
+			case m.syncCh <- m.allGroups():
+			case <-ticker.C:
+				m.metrics.DelayedUpdates.Inc()
+				level.Debug(m.logger).Log("msg", "Discovery receiver's channel was full so will retry the next cycle")
+				select {
+				case m.triggerSend <- struct{}{}:
+				default:
+				}
+			case <-m.ctx.Done():
+				return
+			}
+		case <-m.ctx.Done():
+			return
+		}
+
+		// We restart the ticker to ensure that no two updates are less than updatert apart.
+		ticker.Reset(m.updatert)
+	}
 
 	for {
 		select {
