8ab1ad93aae8e7c22f8189dfa62aff317503b2f4
bwplotka <bwplotka@gmail.com>
2025-08-21 22:34:04 +0100

    chore: build images in the same image as our internal CI for repro
    Signed-off-by: bwplotka <bwplotka@gmail.com>

--- google_vendor.sh
+++ google_vendor.sh
@@ -7,3 +7,16 @@ set -o xtrace
 
 echo "Installing dependencies (Go & npm) with docker..."
 DOCKER_BUILDKIT=1 docker buildx build . -f ./Dockerfile.google --target vendor -o . -t gmp/sync
+
+echo "Early validation for vendored files..."
+dirs_to_check=(
+  "./vendor"
+  "./web/ui/node_modules"
+  "./web/ui/module/codemirror-promql/node_modules"
+)
+for dir in "${dirs_to_check[@]}"; do
+  if [ ! -d "$dir" ]; then
+    echo "FAIL: $dir directory is missing after vendoring...."
+    exit 1
+  fi
+done
