125d5232d37ce5dbc97e006ce51e63a81b7c9305
bwplotka <bwplotka@gmail.com>
2025-12-15 22:30:00 +0000

    chore: rm unused Dockerfile, ported Dockerfile.google and vendor script from 2.53.5
    Signed-off-by: bwplotka <bwplotka@gmail.com>

---
+++ google_vendor.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+set -o xtrace
+
+echo "Installing dependencies (Go & npm) with docker..."
+DOCKER_BUILDKIT=1 docker buildx build . -f ./Dockerfile.google --target vendor -o . -t gmp/sync
+
+echo "Early validation for vendored files..."
+dirs_to_check=(
+  "./vendor"
+  "./web/ui/node_modules"
+)
+for dir in "${dirs_to_check[@]}"; do
+  if [ ! -d "$dir" ]; then
+    echo "FAIL: $dir directory is missing after vendoring...."
+    exit 1
+  fi
+done
